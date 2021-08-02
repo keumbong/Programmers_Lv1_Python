90c2998fdcf5557bd28abd6f548d16196cdbc654
keumbong <87923413+keumbong@users.noreply.github.com>
2021-08-02 22:42:52 +0900

    Add files via upload

---
+++ 숫자 문자열과 영단어.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = s
+    dic = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7',
+           'eight': '8', 'nine': '9'}
+
+    for a, b in dic.items():
+        answer = answer.replace(a, b)
+
+    return int(answer)
