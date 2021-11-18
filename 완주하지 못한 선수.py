34d426ec03fe58aeae474e6ea05fbf59de9ce3e8
keumbong <87923413+keumbong@users.noreply.github.com>
2021-11-18 23:46:05 +0900

    Add files via upload

--- 완주하지 못한 선수.py
+++
@@ -1,8 +0,0 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-
-    for part, comp in zip(participant, completion):
-        if part != comp:
-            return part
-    return participant.pop()
